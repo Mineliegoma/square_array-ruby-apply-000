38be20224a9ce910df2610fe9ba4eede5fb47256
adept-bell-4492 <garlandegoma@gmail.com>
2020-06-24 01:26:49 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,9 +2,9 @@ def square_array(array)
   # your code here
   
   array.each do |number|
-    updated_array=number**2
+    updated_array = number**2
      
   end
-  updated_array = number**2
+  updated_array(array)
 end
 square_array([1,2,3])
